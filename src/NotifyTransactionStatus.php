b0d78f28deefd3d221461267b6846b8fb2392583
wattana <wattana.php@gmail.com>
2021-12-15 12:27:16 +0700

    add util method for check tx status

--- src/NotifyTransactionStatus.php
+++ src/NotifyTransactionStatus.php
@@ -4,6 +4,12 @@ namespace Persec\ShopeePay;
 
 class NotifyTransactionStatus
 {
+    private const STATUS_PROCESSING = 2;
+    private const STATUS_SUCCESS = 3;
+    private const STATUS_FAILED = 4;
+
+    private const TX_TYPE_PAYMENT = 13;
+    private const TX_TYPE_REFUND = 15;
     public $amount;
     public $transaction_sn;
     public $method;
@@ -27,7 +33,22 @@ class NotifyTransactionStatus
         $this->terminal_id = $data['terminal_id'] ?? null;
         $this->co_funding = $data['co_funding'] ?? null;
         $this->reference_id = $data['reference_id'] ?? null;
-        $this->transaction_type = $data['transaction_type'] ?? null;
-        $this->transaction_status = $data['transaction_status'] ?? null;
+        $this->transaction_type = intval($data['transaction_type'] ?? 0);
+        $this->transaction_status = intval($data['transaction_status'] ?? self::STATUS_PROCESSING);
+    }
+
+    public function isTransactionSuccess(): bool
+    {
+        return $this->transaction_status === self::STATUS_SUCCESS;
+    }
+
+    public function isTransactionFailed(): bool
+    {
+        return $this->transaction_status === self::STATUS_FAILED;
+    }
+
+    public function isTypePayment(): bool
+    {
+        return $this->transaction_type === self::TX_TYPE_PAYMENT;
     }
 }
